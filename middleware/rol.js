72a01d3906d4efb2b007ddbc0a25f4595863ac02
Ivan Baron <idbaron17@misena.edu.co>
2022-04-08 20:59:47 -0500

    master

--- middleware/rol.js
+++ middleware/rol.js
@@ -4,17 +4,16 @@ const {handleHttpError} =  require("../utils/handleError")
 /**
  * Array con los lores permitidos
  */
-const checkRol = (role) => (req, res, next) => {
+const checkRol = (roles) => (req, res, next) => {
     try{
         //? Estraemos los datos de la req
-        const {user} = req.params;
+        const {user} = req;
         
-        console.log(user);
         //? Estraemos el rol de los datos del User que por predeterminado es "Invitado"
         const rolesByUser = user.role; //TODO ["invitado"]
         
         //? hace la comparación del rol que tiene el usuario con el que se define en la ruta.
-        const checkValueRol = role.some((rolSingle) => rolesByUser.includes(rolSingle)) // Devuelve un boolean
+        const checkValueRol = roles.some((rolSingle) => rolesByUser.includes(rolSingle)) // Devuelve un boolean
 
         //? Condición en caso de error mostrara 403
         if(!checkValueRol){
