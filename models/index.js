239cbe8a17e43332d976f3ae975f281a85d2d58e
KingJavier <jerey62@gmail.com>
2022-05-12 08:44:27 -0500

    El perezoso trabaja doble

--- models/index.js
+++ models/index.js
@@ -3,7 +3,6 @@ const models ={
     registrosalidaModel: require('./nosql/regsalida'),
     storageModel: require('./nosql/storage'),
     fototempModel: require('./nosql/FotoTemp'),
-    fototempsalModel: require('./nosql/FotoTempSal'),
     userModel: require('./nosql/users'),
     perfilModel: require('./nosql/perfil'),
     certificadoModel: require('./nosql/certificado')
